1555226ba6609f8f4bab41a67e1932e566c22aef
hjq133 <1399615054@qq.com>
2020-09-06 19:07:55 +0800

    add comment

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -4,11 +4,11 @@ using namespace std;
 vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
         map<int, int> a;
-        for(int i=0; i<nums.size(); i++) {
+        for(int i=0; i<nums.size(); i++) {  // 建立hash表，反向查询索引（这种表可以在O(1)时间内判断某个数是否在数组中存在）
             a.insert(pair<int, int>(nums[i], i));
         }
-        for(int i=0; i<nums.size(); i++) {
-            if((a.count(target-nums[i]) > 0) && (a[target-nums[i]] != i)) {
+        for(int i=0; i<nums.size(); i++) {  // 循环遍历，复杂度O(n)
+            if((a.count(target-nums[i]) > 0) && (a[target-nums[i]] != i)) {  // 用hash表进行查找，复杂度为O(1)
                 res.push_back(i);
                 res.push_back(a[target-nums[i]]);
                 break;
