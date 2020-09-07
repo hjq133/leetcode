d4e7193fbfa02e23aeec37c47e4076c04838255c
hjq133 <1399615054@qq.com>
2020-09-07 14:42:47 +0800

    add 11 双指针

---
+++ 11_CWMW.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+    int i = 0, j = height.size() - 1, maxA = 0, h, A;
+    while(i < j) {
+        h = min(height[i], height[j]);
+        A = h * (j - i);
+        maxA = max(maxA, A);
+        if(height[i] < height[j]) {
+            i += 1;
+        }else {
+            j -= 1;
+        }
+    }
+    return maxA;
+}
+
+int main() {
+    vector<int> a = {1,8,6,2,5,4,8,3,7};
+    int area = maxArea(a);
+    cout<< area << endl;
+    return 0;
+}
