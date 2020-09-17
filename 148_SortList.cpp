1d452071f8d577f6753af4967fac4eee03a27c61
hjq133 <1399615054@qq.com>
2020-09-17 23:49:03 +0800

    merge sort list

---
+++ 148_SortList.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* findMid(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    ListNode* previous = NULL;
+    while(fast != NULL && fast->next != NULL) {
+        previous = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    // 把链表分成两个部分
+    previous->next = NULL;
+    return slow;
+}
+
+ListNode* mergeTwoList(ListNode* l1, ListNode* l2) {
+    if(l1 == NULL) return l2;
+    if(l2 == NULL) return l1;
+    if(l1->val < l2->val) {
+        l1->next = mergeTwoList(l1->next, l2);
+        return l1;
+    }else {
+        l2->next = mergeTwoList(l1, l2->next);
+        return l2;
+    }
+}
+
+ListNode* mergeSort(ListNode* head) {
+    if(head->next == NULL) return head;
+    ListNode* mid = findMid(head);
+    ListNode* l1 = mergeSort(head);
+    ListNode* l2 = mergeSort(mid);
+    return mergeTwoList(l1, l2);
+}
+
+ListNode* sortList(ListNode* head) {
+    return (head == NULL) ? NULL: mergeSort(head);
+}
