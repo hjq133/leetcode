ee19ee0a95a9fc2c999176e9056f730b881ffb2e
hjq133 <1399615054@qq.com>
2020-10-08 18:44:16 +0800

    update top k th

--- 常用经典算法/find_kth(partition).py
+++ 常用经典算法/find_kth(partition).py
@@ -1,3 +1,7 @@
+# TODO 注意这是找第k小的代码 0 <= k <= len - 1
+# TODO 如果找第k大，partition符号换一下就ok了
+
+
 def partition(lst, begin, end):
     pivot = lst[begin]
     while begin < end:
@@ -23,7 +27,7 @@ def find_kth(k, lst, begin, end):
 
 lst1 = [11, 3, -4, 10, 100, -99, -1000]
 
-i = 3
+i = 0  # 0 <= i <= len - 1
 res = find_kth(len(lst1) - 1 - i, lst1, 0, len(lst1) - 1)  # 找第i小
 print(res)
 res = find_kth(i, lst1, 0, len(lst1) - 1)  # 找第i大
