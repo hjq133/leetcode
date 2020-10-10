296b551bffd24c5fb22450850b95b7b60b458748
hjq133 <1399615054@qq.com>
2020-10-10 21:25:33 +0800

    update

--- c_code/53_MaximumSubarray.cpp
+++ c_code/53_MaximumSubarray.cpp
@@ -4,19 +4,16 @@ using namespace std;
 // 最大子序列和
 // 解法：动态规划
 // 状态转移方程 ：f i = max ( f i-1 + a i,  a i )
-
+// f i 表示以i结尾的连续子数组的最大和
+// id记录这个数组结尾的位置，然后就可以找出这个数组了
 int maxSubArray(vector<int>& nums) {
-    int pre = 0, maxAns = nums[0];
-    for(auto num : nums) {
-        pre = max(pre + num, num);
-        maxAns = max(maxAns, pre);
+    int f = 0, max_sum = nums[0], id = 0;
+    for(int i=0; i<nums.size(); i++) {
+        f = max(nums[i], nums[i] + f);
+        if(max_sum < f) {
+            id = i;
+            max_sum = f;
+        }
     }
-    return maxAns;
-}
-
-int main() {
-    vector<int> nums= {-2,1,-3,4,-1,2,1,-5,4};
-    int maxs = maxSubArray(nums);
-    cout<<maxs<<endl;
-    return 0;
+    return max_sum;
 }
