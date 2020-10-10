8dfc4f6f0ac0d571e6577182afc0d812fdc279a5
hjq133 <1399615054@qq.com>
2020-10-10 20:08:45 +0800

    update merge 2 list

--- c_code/21_MTSL.cpp
+++ c_code/21_MTSL.cpp
@@ -10,22 +10,13 @@ struct ListNode {
 };
 
 ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    ListNode* res = new ListNode(0);
-    ListNode* l3 = res;
-    while(l1 != NULL && l2 != NULL) {
-        if(l1->val > l2->val) {
-            l3->next = l2;
-            l2 = l2->next;
-        } else {
-            l3->next = l1;
-            l1 = l1->next;
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-        l3 = l3->next;
     }
-    if(l1 == NULL) {
-        l3->next = l2;
-    } else {
-        l3->next = l1;
-    }
-    return res->next;
-}
