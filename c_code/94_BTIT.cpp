dcc48ad263bb9d02f1153270ec9d15643e02ee42
hjq133 <1399615054@qq.com>
2020-10-12 10:36:58 +0800

    add all

--- c_code/94_BTIT.cpp
+++ c_code/94_BTIT.cpp
@@ -17,7 +17,26 @@ void mid_order(TreeNode* root) {
     inorderTraversal(root->right);
 } 
 
+// 递归版本
 vector<int> inorderTraversal(TreeNode* root) {
     mid_order(root);
+    return res;
+}
+
+// 非递归版本 （迭代版本）
+vector<int> inorderTraversal(TreeNode* root) {
+    TreeNode* stack[100];
+    vector<int> res;
+    TreeNode* p = root;
+    int top = -1;
+    if(root != NULL) {
+        do {
+            for(; p != NULL; p=p->left)
+                stack[++top] = p;
+            p = stack[top--];  // 弹栈的时候访问p
+            res.push_back(p->val);  // visit p
+            p = p->right;
+        }while(p != NULL || top != -1);
+    }
     return res;
 }
