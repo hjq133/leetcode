cc5201309648740a3115f4f61a3796dfc669a6ae
hjq133 <1399615054@qq.com>
2020-10-12 11:42:22 +0800

    add all

--- c_code/589_NTPT.cpp
+++ c_code/589_NTPT.cpp
@@ -14,8 +14,33 @@ public:
     }
 };
 
+// 递归算法
+// vector<int> res;
+// void _preorder(Node* root) {
+//     if(root == NULL) return;
+//     res.push_back(root->val);
+//     for(auto tree: root->children) {
+//         preorder(tree);
+//     }
+// }
+
+// vector<int> preorder(Node* root) {
+//     _preorder(root);
+//     return res;
+// }
+
+// 非递归算法
 vector<int> preorder(Node* root) {
-    
+    Node* stack[100];
+    vector<int> res;
+    int top = -1;
+    Node* p = root;
+    if(root != NULL) {
+        for(; p != NULL; p=p->left) {
+            
+        }
+    }
+    return res;
 }
 
 int main() {
