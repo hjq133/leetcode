f02d647173e734563da7ce6d90f75b349f76c572
hjq133 <1399615054@qq.com>
2020-10-13 23:49:29 +0800

    asd

--- c_code/136_SingleNumber.cpp
+++ c_code/136_SingleNumber.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int singleNumber(vector<int>& nums) {
+//     int single = 0;
+//     for(int i=0; i<nums.size(); i++) {
+//         single ^= nums[i];
+//     }
+//     return single;
+// }
+
 int singleNumber(vector<int>& nums) {
-    int single = 0;
+    unordered_map<int, int> m;
+    int result = 0;
     for(int i=0; i<nums.size(); i++) {
-        single ^= nums[i];
+        m[nums[i]] ++;
     }
-    return single;
+    unordered_map<int, int>::iterator it = m.begin();
+    for(; it != m.end(); it++) {
+        if(it->second == 1) return it->first;
+    }
 }
 
 int main() {
